9580f68d1b148fb216b51b06cf59e4a2af9a4c78
Adham Tarek <adhamtarek291@gmail.com>
2025-11-12 20:59:46 +0200

    remove sections

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -5,15 +5,9 @@ import { use } from "react";
 
 import WhatsAppFloat from "@/components/whats-app-float";
 
-import AboutSection from "./components/about-section";
 import ContactSection from "./components/contact-section";
-import ExecutionSection from "./components/execution-section";
-import HeroSection from "./components/hero-section";
 import PricingSection from "./components/pricing-section";
 import ServicesSection from "./components/services-section";
-import SpeedSection from "./components/speed-section";
-import StatsSection from "./components/stats-section";
-import WhyUsSection from "./components/why-us-section";
 
 export default function IndexPage({ params }: PageProps<"/[locale]">) {
   const { locale } = use(params);
@@ -23,12 +17,12 @@ export default function IndexPage({ params }: PageProps<"/[locale]">) {
 
   return (
     <main>
-      <HeroSection />
-      <AboutSection />
-      <StatsSection />
-      <WhyUsSection />
-      <SpeedSection />
-      <ExecutionSection />
+      {/* <HeroSection /> */}
+      {/* <AboutSection /> */}
+      {/* <StatsSection /> */}
+      {/* <WhyUsSection /> */}
+      {/* <SpeedSection /> */}
+      {/* <ExecutionSection /> */}
       <ServicesSection />
       <PricingSection />
       <ContactSection />
